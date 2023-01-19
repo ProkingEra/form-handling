970872b9e470b1dcf04ab76ce8dad96c5094760f
cameron mauzy <cameronmauzy123@gmail.com>
2023-01-19 01:16:19 -0500

    Styling Portion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { Typography } from '@mui/material';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className='App'>
       <Form/>
+      <Button variant="warning"> Click here </Button>
+      ​​​​​​​<Typography variant="h4" gutterBottom>Welcome to React</Typography>
+      <Product/>
     </div>
   );
 }
